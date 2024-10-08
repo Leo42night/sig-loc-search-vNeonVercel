7b5ee6db32b7edc88177af4b71bf32fcdebd11be
Leo42night <109854127+Leo42night@users.noreply.github.com>
2024-10-08 10:38:38 +0700

    initial

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+// sesuaikan nama url
+define('BASEURL', 'http://localhost:8080/mm/public');
+
+// DB
+define('DB_HOST', 'localhost:3306');
+define('DB_NAME', 'poi_db');
+define('DB_USER', 'root');
+define('DB_PASS', '');
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+include './app/config/config.php';
+
+// atur alamat server dan database di app/config/config.php
+// hiasan dari var_dump di datas layar sebagai informasi halaman, tidak apa jika dihapus. letaknya di app/view/templates/header atau app/controllers
+header("Location: " . BASEURL);
+exit;
