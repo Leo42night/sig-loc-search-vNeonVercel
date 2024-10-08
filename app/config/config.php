34f49541e81d9f2045c6eef12e49c8e10a78919b
Leo42night <109854127+Leo42night@users.noreply.github.com>
2024-10-08 11:35:51 +0700

    post gre inside

--- app/config/config.php
+++ app/config/config.php
@@ -8,14 +8,27 @@ if ($_ENV['ENV'] == 'production') {
   $password = $_ENV['PG_PASSWORD'];
   $endpoint = $_ENV['PG_ENDPOINT'];
 
-  $connection_string = "host=" . $host . " port=" . $port . " dbname=" . $db . " user=" . $user . " password=" . $password . " options='endpoint=" . $endpoint . "' sslmode=require";
-
-  $dbconn = pg_connect($connection_string);
-
-  if (!$dbconn) {
-    die("Connection failed: " . pg_last_error());
+  try {
+    $dsn = "pgsql:host=$host;port=$port;dbname=$db;options='--endpoint=$endpoint';sslmode=require";;
+    
+    // make a database connection
+    $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+  
+    if ($pdo) {
+      echo "Connected to the $db database successfully with SSL!";
+    }
+  } catch (PDOException $e) {
+    die("Connection failed: ".$e->getMessage());
   }
-  echo "Connected successfully";
+
+  // $connection_string = "host=" . $host . " port=" . $port . " dbname=" . $db . " user=" . $user . " password=" . $password . " options='endpoint=" . $endpoint . "' sslmode=require";
+
+  // $dbconn = pg_connect($connection_string);
+
+  // if (!$dbconn) {
+  //   die("Connection failed: " . pg_last_error());
+  // }
+  // echo "Connected successfully";
 } else {
   // sesuaikan nama url
   define('BASEURL', 'http://localhost:8080/mm/public');
