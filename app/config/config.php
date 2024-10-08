8bed5bf264495df2bee8d3feb33da6cea70561a0
Leo42night <109854127+Leo42night@users.noreply.github.com>
2024-10-08 11:24:11 +0700

    new test inside folder

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,28 @@
-<?php 
+<?php
 
-// sesuaikan nama url
-define('BASEURL', 'http://localhost:8080/mm/public');
+if ($_ENV['ENV'] == 'production') {
+  $host = $_ENV['PG_HOST'];
+  $port = $_ENV['PG_PORT'];
+  $db = $_ENV['PG_DB'];
+  $user = $_ENV['PG_USER'];
+  $password = $_ENV['PG_PASSWORD'];
+  $endpoint = $_ENV['PG_ENDPOINT'];
 
-// DB
-define('DB_HOST', 'localhost:3306');
-define('DB_NAME', 'poi_db');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+  $connection_string = "host=" . $host . " port=" . $port . " dbname=" . $db . " user=" . $user . " password=" . $password . " options='endpoint=" . $endpoint . "' sslmode=require";
+
+  $dbconn = pg_connect($connection_string);
+
+  if (!$dbconn) {
+    die("Connection failed: " . pg_last_error());
+  }
+  echo "Connected successfully";
+} else {
+  // sesuaikan nama url
+  define('BASEURL', 'http://localhost:8080/mm/public');
+  
+  // DB
+  define('DB_HOST', 'localhost:3306');
+  define('DB_NAME', 'poi_db');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+}
